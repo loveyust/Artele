e10edcaa802c355c1a057ff68f81d343bf1d880c
loveyust <loveyust@gmail.com>
2020-05-07 10:38:00 -0700

    Fix untracked files

---
+++ srcs/environment.js
@@ -0,0 +1,15 @@
+export const environment = {
+  // production env params
+  production: {
+    socketPort: 3001,
+    airtableBase: 'app53VoYIdzAsqcGG',
+    airtableKey: 'keyxBf96Jljdl8St6'
+  },
+
+  // development env params
+  development: {
+    socketPort: 3001,
+    airtableBase: 'app53VoYIdzAsqcGG',
+    airtableKey: 'keyxBf96Jljdl8St6'
+  }
+};
